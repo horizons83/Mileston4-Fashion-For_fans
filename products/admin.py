96f390f9b44798ec9517c9382cdf32a75b7a1843
horizons83 <euanfennell@icloud.com>
2021-07-31 19:52:01 +0000

    Created products app and created database models Category, Team, Product

---
+++ products/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Product, Team, Category
+
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(Team)
+admin.site.register(Category)
