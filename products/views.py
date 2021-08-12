0449b4f4abc83f5e92ca1938b44f905d9c0816c0
horizons83 <euanfennell@icloud.com>
2021-08-12 20:40:14 +0000

    Created product detsild template

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Team
 # Create your views here.
 
 
 def all_products(request):
+    """ A view to show all products """
 
     products = Product.objects.all()
 
@@ -12,3 +13,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+    team = Team.objects.all()
+
+    context = {
+        'product': product,
+        'team': team,
+    }
+
+    return render(request, 'products/product_detail.html', context)
