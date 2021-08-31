88f0ad3f936a8f5df1a090da234d18b39ae0666a
horizons83 <euanfennell@icloud.com>
2021-08-31 22:43:30 +0000

    Created add_product.html and urls for product management

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, Category, Team
+from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
@@ -11,10 +11,8 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        teams = Team.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-        friendly_names = [(t.id, t.get_friendly_name()) for t in teams]
 
-        self.fields['category', 'team'].choices = friendly_names
+        self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
