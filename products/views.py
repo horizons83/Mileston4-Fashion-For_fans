9e252722e9025e50a93f7429e52f163f60c952da
horizons83 <euanfennell@icloud.com>
2021-09-03 21:36:52 +0000

    created delete product view and url to allow admin to delete products from the store, added edit and delete links to products.html and product_detail.html

--- products/views.py
+++ products/views.py
@@ -78,9 +78,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Your product was added successfully!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Whoops product not added. Please check the form is valid.')
     else:
@@ -119,3 +119,14 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """
+    A View to allow admin to delete
+    products from the store
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product was successfully deleted!')
+    return redirect(reverse('products'))
