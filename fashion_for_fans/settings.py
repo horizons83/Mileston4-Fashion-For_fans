04a4352bba02d3bcadf05070c30a3d4f333a2429
horizons83 <euanfennell@icloud.com>
2021-09-04 21:23:01 +0000

    Removed secret key and set debug

--- fashion_for_fans/settings.py
+++ fashion_for_fans/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=8wqph%5sq(7j(*0zp&1bu$%r^nzc61g)+uqb94hh9rjq*9dj6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['milestone4-fashion-for-fans.herokuapp.com', 'localhost']
 
