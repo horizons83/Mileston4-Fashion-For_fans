161e600123f518cf66e0905f17e6dbe30293bd37
horizons83 <euanfennell@icloud.com>
2021-09-06 15:22:38 +0000

    created add blog post url and form

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('', views.blog, name='blog'),
     path('<int:blogpost_id>/',
          views.blog_detail,
-         name='blog_detail'),
+         name='blog_detail'
+         ),
+    path('add/',
+         views.add_blogpost,
+         name='add_blogpost'
+         ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import BlogPost, BlogComment
+from .forms import BlogForm, CommentForm
 
 # Create your views here.
 
@@ -33,3 +35,36 @@ def blog_detail(request, blogpost_id):
     }
 
     return render(request, 'blog/blog_detail.html', context)
+
+
+@login_required
+def add_blogpost(request):
+    """
+    Create Add blog post form for admin only.
+    """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES)
+        if form.is_valid():
+            blogpost = form.save(commit=False)
+            blogpost.author = request.user
+            blogpost = form.save()
+            messages.success(request, 'Successfully added blog post!')
+            return redirect(reverse('blog_detail', args=[blogpost.id]))
+        else:
+            messages.error(
+                        request,
+                        'Failed to add blog post.\
+                        Please ensure the form is valid.')
+    else:
+        form = BlogForm()
+        template = 'blog/add_blogpost.html'
+        context = {
+            'form': form,
+        }
+
+    return render(request, template, context)
