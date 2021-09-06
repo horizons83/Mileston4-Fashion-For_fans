8f252fe5547715fc00364c926c5f6e922b26c810
horizons83 <euanfennell@icloud.com>
2021-09-06 13:19:50 +0000

    Created blog and blog_detail.html and styled blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name='blog')
+    path('', views.blog, name='blog'),
+    path('<int:blogpost_id>/',
+         views.blog_detail,
+         name='blog_detail'),
 ]
