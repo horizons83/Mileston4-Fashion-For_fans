49b4f421915ed04e454000189c55343d8025a6df
horizons83 <euanfennell@icloud.com>
2021-09-06 10:06:34 +0000

    Created blog app, and blog model

--- fashion_for_fans/settings.py
+++ fashion_for_fans/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'blog',
 
     # Other
     'crispy_forms',
@@ -126,10 +127,8 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+            'NAME': BASE_DIR / 'db.sqlite3', }
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
