2c7264f212ae98bcf56497cd122ac86216f8c21d
horizons83 <euanfennell@icloud.com>
2021-09-07 16:18:12 +0000

    Set admin_emal to default in contact views

--- contact/views.py
+++ contact/views.py
@@ -32,7 +32,7 @@ def contact(request):
             )
 
             if settings.EMAIL_HOST_USER:
-                admin_email = settings.EMAIL_HOST_USER
+                admin_email = settings.DEFAULT_FROM_EMAIL
             else:
                 admin_email = settings.DEFAULT_FROM_EMAIL
 
